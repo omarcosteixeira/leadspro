1206b58061cecacfa9c82c58682b4312132df110
Marcos Teixeira <o_marcosteixeira@icloud.com>
2026-07-10 13:07:25 -0300

    refactor: simplify firestore rules and cleanup
    Simplified Firestore access patterns by removing redundant `.get()` calls and removed unnecessary Firebase configuration/utility scripts that are no longer required.

--- test_query.cjs
+++
@@ -1,6 +0,0 @@
-const { initializeApp, cert } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
-
-// We don't have the service account for the principal project.
-// So we can't easily query as the user.
-console.log("Can't test without service account");
